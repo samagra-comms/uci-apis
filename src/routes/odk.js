2a3619e581a4911858192ceb60ff15f72adda38a
ChakshuGautam <chaks.gautam@gmail.com>
2021-10-12 16:47:51 +0530

    Fix: Issues with ODK service url

--- src/routes/odk.js
+++ src/routes/odk.js
@@ -20,19 +20,16 @@ var storage = multer.diskStorage({
 const upload = multer({ storage: storage });
 const uuid = require("uuid/v1");
 const BASE_URL = "/admin/v1";
-const ODK_BASE_URL = process.env.ODK_BASE_URL;
+const ODK_BASE_URL = process.env.ODK_SERVICE;
 const messageUtils = require("../service/messageUtil");
 const OdkMessages = messageUtils.ODK;
 const programMessages = messageUtils.PROGRAM;
 const errorCode = messageUtils.ERRORCODES;
 
 async function uploadForm(req, res) {
-  // const rspObj = req;
   const errCode =
     programMessages.EXCEPTION_CODE + "_" + OdkMessages.UPLOAD.EXCEPTION_CODE;
-  const vault = new Vault();
-  let credentials = vault.getCredentials("", { variable: "ODK" });
-  credentials = {
+  const credentials = {
     username: process.env.ODK_USERNAME,
     password: process.env.ODK_PASSWORD,
   };
@@ -48,7 +45,6 @@ async function uploadForm(req, res) {
   );
   getRequest.request(
     function (data) {
-      //console.log("req:",rspObj)
       var formData = new FormData();
       const file = fs.createReadStream(req.file.path);
       formData.append("form_def_file", file, req.file.originalname);
