f2b4f238a53b8364ff8fa21a49225b8b109d62af
Radhay <radhay@samagragovernance.in>
2022-07-05 16:12:37 +0000

    Fix: Issues with auth guard accepting all requests

--- v2/uci/src/auth/auth.guard.ts
+++ v2/uci/src/auth/auth.guard.ts
@@ -25,21 +25,29 @@ export class JwtAuthGuard extends AuthGuard('jwt') implements IAuthGuard {
   public async canActivate(context: ExecutionContext): Promise<boolean> {
     await super.canActivate(context);
     const request: Request = context.switchToHttp().getRequest();
-    const token = request.headers['authorization'].split(' ')[1];
+    let token = "";
+    if (request.headers['authorization']) {
+      token = request.headers['authorization'].split(' ')[1];
+    }
     let tokenData: any;
     const isAdmin =
       request.headers['admin-token'] !== undefined &&
       request.headers['admin-token'] === this.adminToken;
     request.headers['isAdmin'] = isAdmin;
+    console.log(`isAdmin: ${isAdmin}`);
+    if(isAdmin) return true; //Break circuit early.
     try {
-      tokenData = this.jwtService.verify(token);
       let userId;
-      if (this.instance === 'sunbird') {
-        userId = tokenData.sub.split(':')[tokenData.sub.split(':').length - 1];
-      } else {
-        userId = tokenData.sub;
+      if(token !== "") {
+        tokenData = this.jwtService.verify(token);
+        if (this.instance === 'sunbird') {
+          userId = tokenData.sub.split(':')[tokenData.sub.split(':').length - 1];
+        } else {
+          userId = tokenData.sub;
+        }
       }
       // Either the user is an admin or the user is the owner of the resource
+      if(userId === undefined) return false; //undefined userId means invalid token
       return userId === request.headers['ownerid'] || isAdmin;
     } catch (e) {
       console.log(e);
