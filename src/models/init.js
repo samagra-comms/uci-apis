8348ce2122c7bb5f30414c175463f42832f10826
subhash_chandra_budde <subhash.chandra.budde@oracle.com>
2023-04-05 22:49:39 +0530

    Changes for postgres ssl configuration

--- src/models/init.js
+++ src/models/init.js
@@ -29,6 +29,7 @@ const knexConfig = {
       user: process.env.POSTGRES_USER,
       password: process.env.POSTGRES_PASSWORD,
       database: process.env.POSTGRES_DB,
+      ssl: { rejectUnauthorized: false }
     },
     pool: {
       min: 2,
