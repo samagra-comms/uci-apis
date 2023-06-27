39ca1cda785eaf599def0dc9ddd295f61f1642b9
Chinmoy Chakraborty <chinmoy12c@gmail.com>
2023-06-27 12:57:50 +0530

    Removed obselete files.

--- v2/uci/src/global-services/prisma.service.mock.ts
+++
@@ -1,9 +0,0 @@
-import { PrismaClient } from 'prisma/generated/prisma-client-js';
-import { Context, createMockContext } from './prisma.context';
-const mockCtx = createMockContext();
-const ctx = mockCtx as unknown as Context;
-
-const PrismaServiceMock: PrismaClient =
-  mockCtx.prisma as unknown as PrismaClient;
-
-export { PrismaServiceMock };
