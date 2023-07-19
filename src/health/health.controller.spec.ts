05e778d9941554d4a68e8818f419908ffaf1f2c0
Varun Agarwal <varunagarwal10.2@gmail.com>
2023-07-20 01:16:29 +0530

    added healthController specs

--- src/health/health.controller.spec.ts
+++ src/health/health.controller.spec.ts
@@ -1,17 +1,67 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { HealthController } from './health.controller';
+import { HealthService } from './health.service';
+import { HealthCheckResult } from '@nestjs/terminus';
 
 describe('HealthController', () => {
   let controller: HealthController;
 
+  const mockHealthService = {
+    checkHealth: jest.fn().mockResolvedValue({
+      status: 'ok',
+      details: {
+        'UCI-API': {
+          status: 'up',
+        },
+      },
+    } as HealthCheckResult),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [HealthController],
-    }).compile();
+      providers: [HealthService],
+    })
+      .overrideProvider(HealthService)
+      .useValue(mockHealthService)
+      .compile();
 
     controller = module.get<HealthController>(HealthController);
   });
 
+  describe('getServiceHealth', () => {
+    it('should return service health check result', async () => {
+      const mockResult: HealthCheckResult = {
+        status: 'ok',
+        details: {
+          'UCI-API': {
+            status: 'up',
+          },
+        },
+      };
+
+      const result = await controller.getServiceHealth();
+      expect(result).toEqual(mockResult);
+    });
+  });
+
+  describe('checkHealth', () => {
+    it('should return health check result', async () => {
+      const mockBody: any = {};
+      const mockResponse = {
+        status: 'ok',
+        details: {
+          'UCI-API': {
+            status: 'up',
+          },
+        },
+      };
+      const result = await controller.checkHealth(mockBody);
+
+      expect(result).toEqual(mockResponse);
+      expect(mockHealthService.checkHealth).toBeCalled;
+    });
+  });
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
