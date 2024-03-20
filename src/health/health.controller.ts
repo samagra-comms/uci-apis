c622d1c71b2f74bf33028e5a8cee27e1304c7569
Anvansh <32684077+RyanWalker277@users.noreply.github.com>
2024-03-20 19:01:55 +0530

    Made health endpoint public (#238)

--- src/health/health.controller.ts
+++ src/health/health.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Get } from '@nestjs/common';
 import { HealthService } from './health.service';
 import { HealthCheckResult } from '@nestjs/terminus';
+import { Public } from '../auth/public.decorator';
 
 @Controller('health')
 export class HealthController {
@@ -8,11 +9,13 @@ export class HealthController {
         private readonly healthService: HealthService
     ) {}
 
+    @Public() 
     @Get()
     async checkHealth(@Body() body: any): Promise<HealthCheckResult> {
         return this.healthService.checkHealth();
     }
 
+    @Public() 
     @Get('/ping')
     async getServiceHealth(): Promise<HealthCheckResult> {
         const resp: HealthCheckResult = {
