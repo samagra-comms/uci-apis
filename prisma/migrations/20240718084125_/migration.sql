567934de0285fc01528925959bad4801eff4e40d
Chinmoy Chakraborty <chinmoy12c@gmail.com>
2024-07-18 14:26:53 +0530

    Enable persistent notification scheduling.

---
+++ prisma/migrations/20240718084125_/migration.sql
@@ -0,0 +1,2 @@
+-- DropIndex
+DROP INDEX "Schedules_botId_key";
--- src/modules/bot/bot.service.spec.ts
+++ src/modules/bot/bot.service.spec.ts
@@ -104,6 +104,9 @@ const MockPrismaService = {
       }
     },
   },
+  schedules: {
+    upsert: jest.fn(),
+  }
 }
 
 class MockConfigService {
@@ -885,6 +888,17 @@ describe('BotService', () => {
       expect(id.startsWith('notification_')).toBe(true);
       expect(cron).toStrictEqual(MockCronJob);
     });
+    jest.spyOn(MockPrismaService.schedules, 'upsert').mockImplementation((filter) => {
+      expect(filter.where.id).toBeDefined();
+      expect(filter.create).toStrictEqual({
+        authToken: 'mockToken',
+        botId: 'mockBotId',
+        scheduledAt: futureDate,
+        config: {
+          'myVar': 'myVal',
+        },
+      });
+    });
     await botService.scheduleNotification(
       'mockBotId',
       futureDate,
@@ -894,5 +908,6 @@ describe('BotService', () => {
       'mockToken',
     );
     expect(MockCronJob.start).toHaveBeenCalledTimes(1);
+    expect(MockPrismaService.schedules.upsert).toHaveBeenCalledTimes(1);
   });
 });
